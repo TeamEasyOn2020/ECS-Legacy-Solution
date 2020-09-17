18f15a936db4580d1ac25c26cd358e98456b1efe
Andreas Hermann <andreashermann170@gmail.com>
2020-09-17 11:11:18 +0200

    Testable project

---
+++ ECS.Testable/ITempSensor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ECS.Testable
+{
+    public interface ITempSensor
+    {
+        public IRandomNumberGenerator {private get; private set;}
+
+
+        public int GetTemp();
+        public bool RunSelfTest();
+    }
+}
---
+++ ECS.Testable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECS.Testable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
